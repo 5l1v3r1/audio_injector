d6f9a9c19495a72f8048f046e991d62252a478c1
Mohammad Askar <m.askar@isecur1ty.org>
2016-11-06 00:50:26 +0200

    Update audio_injector.py

--- audio_injector.py
+++ audio_injector.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import binascii,sys
+import binascii,sys,time
 
 if len(sys.argv) != 4:
   print "Usage : python audio_injector.py [template_image] [template_audio_file] [final_image_file]"
@@ -11,21 +11,35 @@ image_template = sys.argv[1] # image file that you want to inject the audio to i
 injected_image = sys.argv[3] # final image file that contains the injected audio file.
 
 def read_audio_file():
- audio_file = open(audio_file_path,"rb")
- data = binascii.hexlify(audio_file.read()) 
- return data
+ try:	
+  audio_file = open(audio_file_path,"rb")
+  data = binascii.hexlify(audio_file.read())
+  return data
+ except:
+	 print "[!]Error while reading audio file" 
+         sys.exit(0)
 
 def inject_audio_to_image(image_path):
  audio_data = read_audio_file()
- image_file = open(image_path,"rb")
- data = binascii.hexlify(image_file.read())
+ try:
+  image_file = open(image_path,"rb")
+  data = binascii.hexlify(image_file.read())
+  print "[+]Offset is : {0}".format(len(data))
+ except:
+  print "[!]Error reading template image"
  data3 = data + read_audio_file()
  bin_data = binascii.unhexlify(data3)
- fi = open(injected_image,"wb")
- fi.write(bin_data)
- fi.close() 
+ try:
+  fi = open(injected_image,"wb")
+  fi.write(bin_data)
+  fi.close()
+  return True
+ except:
+   print "[!]Error writing the injected image"
 
 
-read_audio_file()
-inject_audio_to_image(image_template)
+#read_audio_file()
 
+if inject_audio_to_image(image_template):
+ print "[+]Done !"
+ print "[+]Check {0} file ..".format(injected_image)
