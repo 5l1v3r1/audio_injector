d2d0f9159535f483f2e72c504de77927b3a08e7d
Mohammad Askar <m.askar@isecur1ty.org>
2016-11-06 00:37:37 +0200

    Update audio_extractor.py

--- audio_extractor.py
+++ audio_extractor.py
@@ -2,16 +2,17 @@
 
 import binascii,sys
 
-if len(sys.argv) != 3:
-  print "Usage : python audio_extractor.py [injected_image_file] [audio_file_result]"
+if len(sys.argv) != 4:
+  print "Usage : python audio_extractor.py [injected_image_file] [audio_file_result] [final_offset]"
   sys.exit(0)
 injected_image = sys.argv[1]
 extracted_audio_result = sys.argv[2]
+final_offset = sys.argv[3]
 def extract_audio():
  open_image_file = open(injected_image,"rb")
  data = open_image_file.read()
  hex_data = binascii.hexlify(data)
- audio_file_hex_data = hex_data[640362::]
+ audio_file_hex_data = hex_data[int(final_offset)::]
  binary_data_for_audio_file = binascii.unhexlify(audio_file_hex_data)
  write_audio_file = open(extracted_audio_result,"wb")
  write_audio_file.write(binary_data_for_audio_file)
